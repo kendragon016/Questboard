a2f85061af7ffcf4fcf56acb0d0f1f4e07c1bd3f
Jacob Simon G. Bernardo <jacobsimonbernardo@gmail.com>
2021-04-08 03:24:43 +0800

    added Edit Quest functionality

--- quests/urls.py
+++ quests/urls.py
@@ -7,8 +7,10 @@ urlpatterns = [
     path('home', home_view, name='home'),
     path('create', create_view, name='create'),
     path('list', list_view, name='list'),
-    path('edit/<int:pk>', edit_view, name='edit'),
+    path('edit_board/<int:pk>', edit_board, name='edit_board'),
+    path('edit_quest/<int:pk>', edit_quest, name='edit_quest'),
     path('add/<int:pk>', add_view, name='add'),
     path('board/<str>/<int:pk>', board_view, name='board'),
-    path('sign_up/<int:cpk>/<int:qpk>', sign_up, name='sign_up'),
+    path('sign_up/<int:pk>', sign_up, name='sign_up'),
+    path('confirm_dibs/<str>/<int:pk>', confirm_dibs, name='confirm_dibs'),
 ]
--- quests/views.py
+++ quests/views.py
@@ -25,7 +25,7 @@ def create_view(request):
                 stars=stars
             )
             obj.save()
-            
+
             go_to = '/board/teacher/' + str(obj.pk)
             return redirect(go_to)
 
@@ -40,7 +40,7 @@ def list_view(request):
     return render(request, 'list.html', {'questboard_list': boards})
 
 
-def edit_view(request, pk):
+def edit_board(request, pk):
     obj = Questboard.objects.get(id=pk)
     form = QuestboardForm(instance=obj)
 
@@ -50,6 +50,21 @@ def edit_view(request, pk):
             form.save()
 
             go_to = '/board/teacher/' + str(pk)
+            return redirect(go_to)
+
+    return render(request, 'edit.html', {'form': form})
+
+
+def edit_quest(request, pk):
+    obj = Quest.objects.get(id=pk)
+    form = QuestForm(instance=obj)
+
+    if request.method == "POST":
+        form = QuestForm(request.POST, instance=obj)
+        if form.is_valid():
+            form.save()
+
+            go_to = '/board/teacher/' + str(obj.board.pk)
             return redirect(go_to)
 
     return render(request, 'edit.html', {'form': form})
@@ -95,22 +110,27 @@ def board_view(request, str, pk):
         )
 
 
-def sign_up(request, cpk, qpk):
+def confirm_dibs(request, str, pk):
+    obj = Quest.objects.get(pk=pk)
+    dibs_list = []
+    if obj.sign_ups:
+        for i in obj.sign_ups:
+            dibs_list.append(i)
+    dibs_list.append(str)
+
+    obj.sign_ups = dibs_list
+    obj.save()
+
+    go_to ='/board/student/' + "% s" % obj.board.pk
+    return redirect(go_to)
+
+
+
+def sign_up(request, pk):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data['name']
-            obj = Quest.objects.get(pk=qpk)
+            obj = Quest.objects.get(pk=pk)
 
-            dibs_list = []
-            if obj.sign_ups:
-                for i in obj.sign_ups:
-                    dibs_list.append(i)
-            dibs_list.append(name)
-
-            obj.sign_ups = dibs_list
-            obj.save()
-
-            go_to ='/board/student/' + str(cpk)
-            return redirect(go_to)
-
+            return render(request, 'confirm.html', {'name': name, 'quest':obj})
